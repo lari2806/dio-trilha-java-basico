ee30da19c263d5c24931c3f9b5474b829d454598
Larissa <larissa.rfs28@gmai.com>
2024-07-19 12:21:34 -0300

    Separação nos arquivos para ficar mais organiiado

--- Anatomia-De-Classes/Metodos.java
+++ Anatomia-De-Classes/Metodos.java
@@ -1,16 +1,26 @@
 public class Metodos {
     public static void main(String[] args) {
-    String animal = "cachorro";
-    String nome = "spaik";
-    int idade2 = 6;
-    String meuAnimal = meuAnimal(animal, nome, idade2);
+        String meuNome = "Larissa";
+        int minhaIdade = 17;
+        final String NACIONALIDADE = "brasileira";
+    
+        String animal = "cachorro";
+        String nome = "spaik";
+        int idade2 = 6;
+        
+        String meuAnimal = meuAnimal(animal, nome, idade2);
+        String sobreMim = sobreMim(meuNome, minhaIdade, NACIONALIDADE);
 
-    System.out.println(meuAnimal);
+        System.out.println(sobreMim);
+
+        System.out.println(meuAnimal);
+    }
+    public static String sobreMim(String meuNome, int minhaIdade, final String NACIONALIDADE){
+        return "Olá, meu nome é " + meuNome + " tenho " + minhaIdade + " anos e sou " + NACIONALIDADE + ".";
     }
 
-    
-public static String meuAnimal( String animal, String nome, int idade2){
-    return "Eu tenho um " + animal + " ele se chama " + nome + " e tem " + idade2 + " anos.";
+    public static String meuAnimal( String animal, String nome, int idade2){
+        return "Eu tenho um " + animal + " ele se chama " + nome + " e tem " + idade2 + " anos.";
 
 }
 }
--- Anatomia-De-Classes/Variaveis.java
+++ Anatomia-De-Classes/Variaveis.java
@@ -1,4 +1,4 @@
-public class MetodosEVariaveis{
+public class Variaveis {
     public static void main(String[] args) {
         String meuNome = "Larissa";
         int idade = 17;
@@ -6,15 +6,8 @@ public class MetodosEVariaveis{
         String animal = "cachorro";
         String nome = "spaik";
         int idade2 = 6;
-        String meuAnimal = meuAnimal(animal, nome, idade2);
-
         System.out.println("Olá, meu nome é " + meuNome + " tenho " + idade + " anos e sou " + NACIONALIDADE + ".");
-        System.out.println(meuAnimal);
-        
-    }
 
-    public static String meuAnimal( String animal, String nome, int idade2){
-        return "Eu tenho um " + animal + " ele se chama " + nome + " e tem " + idade2 + " anos.";
-
+        System.out.println( "Eu tenho um " + animal + " ele se chama " + nome + " e tem " + idade2 + " anos.");
     }
-} 
+}
