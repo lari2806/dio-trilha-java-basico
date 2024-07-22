51ba947a6270578dde5da6ce77ef47f398dc8123
Larissa <larissa.rfs28@gmai.com>
2024-07-21 21:32:56 -0300

    Organização arquivos java.

---
+++ TiposEVariaveis/src/TipoTernario.java
@@ -0,0 +1,7 @@
+public class TipoTernario {
+    public static void main(String[] args) {
+        int nota = 6 ;
+        String resultado = nota >= 6 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
