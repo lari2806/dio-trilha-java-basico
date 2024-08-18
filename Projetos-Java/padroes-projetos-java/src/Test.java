249e6ef74581a77a2d34a028dea9483dd681cb63
Larissa <larissa.rfs28@gmai.com>
2024-08-18 11:00:03 -0300

    Modificando arquivo.

---
+++ Projetos-Java/padroes-projetos-java/src/Test.java
@@ -0,0 +1,41 @@
+import dio.gof.singleton.SingletonEager;
+import dio.gof.singleton.SingletonLazy;
+import dio.gof.singleton.SingletonLazyHolder;
+import dio.gof.strategy.Comportamento;
+import dio.gof.strategy.ComportamentoAgressivo;
+import dio.gof.strategy.ComportamentoDefensivo;
+import dio.gof.strategy.ComportamentoNormal;
+import dio.gof.strategy.Robo;
+
+public class Test {
+    public static void main(String[] args) {
+        //Singleton
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyholder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyholder);
+
+        //Strategy
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(agressivo);
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+
+        robo.setComportamento(normal);
+        robo.mover();
+
+
+
+    }
+
+}
