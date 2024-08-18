daab12d969eab1d40308bff37504ec8d39c5f9d2
Larissa <larissa.rfs28@gmai.com>
2024-08-18 10:30:53 -0300

    Adicionando novo arquivo Singleton.

---
+++ Projetos-Java/padroes-projetos-java/src/dio/gof/singleton/SingletonEager.java
@@ -0,0 +1,21 @@
+package dio.gof.singleton;
+
+
+/**
+ * Singleton "apressado"
+ * 
+ * @author Larissa
+ */
+public class SingletonEager {
+
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager(){
+        super();
+    }
+    
+    public static SingletonEager getInstancia(){
+        return instancia;
+    }
+
+}
