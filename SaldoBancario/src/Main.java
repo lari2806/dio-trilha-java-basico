50e3f17d5f35d9051360283bd1625c78fb8b40b2
Larissa <larissa.rfs28@gmail.com>
2025-02-15 14:14:53 -0300

    adicionando arquivo "main" ao repositório

---
+++ SaldoBancario/src/Main.java
@@ -0,0 +1,36 @@
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma: 
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.  
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Entrada do saldo inicial
+        double saldoInicial = scanner.nextDouble();
+
+        // TODO: Na linha abaixo, implemente a entrada das três transações:
+        double transacao = scanner.nextDouble();
+        
+        double transacao2 = scanner.nextDouble();
+      
+        double transacao3 = scanner.nextDouble();
+        
+
+
+
+
+        
+        
+        // TODO: Na linha abaixo, realize o cálculo do saldo final:
+        double saldoFinal = saldoInicial + transacao + transacao2 + transacao3; ;
+        
+        // Saldo final
+        System.out.printf("%.2f\n", saldoFinal);
+
+        scanner.close();
+    }
+}
