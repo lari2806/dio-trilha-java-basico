602372c5ab92ac73d62139628865458a44e686fb
Larissa <larissa.rfs28@gmail.com>
2025-02-18 19:39:36 -0300

    retirando comentários de comando de desafio.

--- SaldoBancario/src/SaldoBancario.java
+++ SaldoBancario/src/SaldoBancario.java
@@ -1,7 +1,3 @@
-// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma: 
-// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
-// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.  
-
 import java.util.Scanner;
 
 public class SaldoBancario {
@@ -9,10 +5,10 @@ public class SaldoBancario {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Entrada do saldo inicial
+
         double saldoInicial = scanner.nextDouble();
 
-        // TODO: Na linha abaixo, implemente a entrada das três transações:
+
         double transacao = scanner.nextDouble();
         
         double transacao2 = scanner.nextDouble();
@@ -20,11 +16,8 @@ public class SaldoBancario {
         double transacao3 = scanner.nextDouble();
         
 
-
-        // TODO: Na linha abaixo, realize o cálculo do saldo final:
         double saldoFinal = saldoInicial + transacao + transacao2 + transacao3; ;
         
-        // Saldo final
         System.out.printf("%.2f\n", saldoFinal);
 
         scanner.close();
