7991515449c8993f3c939d9291dbd537ac4e05da
Larissa <larissa.rfs28@gmail.com>
2025-05-03 13:32:03 -0300

    Adicionando novas classes java ao repositório.

---
+++ get-started/Herancas-Polimorfismo-Java/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+
+        Employee manager =  new Manager();
+
+        manager.setName("Larissa");
+
+        System.out.println(manager.getName());
+    }
+}
